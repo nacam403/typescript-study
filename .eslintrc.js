dad27cb8a468598718209cf966de347d2b9f7daf
nacam403 <satnakam@gmail.com>
2020-04-25 15:26:20 +0900

    ESLintとPrettierをインストール

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    jest: true,
+    browser: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // 必要に応じて追加していく。
+  },
+};
