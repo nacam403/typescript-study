9fe0abb8d5aca2dc3a9a35e07563ddf4cb61c4da
nacam403 <satnakam@gmail.com>
2020-09-12 20:34:31 +0900

    VPCスタック

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
+
+import { env } from "../lib/config";
 import { VpcStack } from "../lib/vpc-stack";
 
 const app = new cdk.App();
-new VpcStack(app, "VpcStack");
+
+new VpcStack(app, "VpcStack", { env });
